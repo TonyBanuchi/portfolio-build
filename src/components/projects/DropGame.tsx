1900d359107d5896458c4c313c8608be9577b6aa
Chimaera <banuchi87@gmail.com>
2025-03-26 11:39:31 -0400

    Applied global styles to dropgame component.

--- src/components/projects/DropGame.tsx
+++ src/components/projects/DropGame.tsx
@@ -1,19 +1,32 @@
 // Style Imports
-import UnderConstruction from '../under-construction/UnderConstruction'
-import styles from './DropGame.module.scss'
+import UnderConstruction from "../under-construction/UnderConstruction";
+import styles from "./DropGame.module.scss";
 
-function DropGame(){
-  return(
+function DropGame() {
+  return (
     <>
-    <h1 className="title">Drop Game</h1>
-    <p className="description">The object of the game is to make it from one corner of the game grid to the opposite corner without being dropped.</p>
-    <p className="description">You can move forward, backward, left, or right, to any "Available" space.</p>
-    <p className='description'>Then its the System's turn. The System will open a random space. If you are on that space, you ... get ...... DROPPED!</p>
-    <br />
-    <UnderConstruction />
+      <div className="flex flex-col"><div className="title">
+        <h1 className={styles.title}>Drop Game</h1>
+      </div>
+      <div className="content">
+        <p className={styles.description}>
+          The object of the game is to make it from one corner of the game grid
+          to the opposite corner without being dropped.
+        </p>
+        <p className={styles.description}>
+          You can move forward, backward, left, or right, to any "Available"
+          space.
+        </p>
+        <p className={styles.description}>
+          Then its the System's turn. The System will open a random space. If
+          you are on that space, you ... get ...... DROPPED!
+        </p>
+      </div>
+      <br />
+      <UnderConstruction />
+      </div>
     </>
-  )
-
+  );
 }
 
-export default DropGame
+export default DropGame;
