c537cbcc0a25a492d6729569f612e6779064b4ef
Chimaera <banuchi87@gmail.com>
2025-03-28 12:14:31 -0400

    Updated project link urls

--- src/components/projects/projects-page/Projects.tsx
+++ src/components/projects/projects-page/Projects.tsx
@@ -4,38 +4,45 @@ import styles from './Projects.module.scss'
 function Projects(){
   return (
     <>
+    <h1 className={styles.title}>Project Repositories</h1>
     <section id="projects">
+    <a href="https://github.com/TonyBanuchi/freeCodeCamp-Projects"
+      className={`${styles['project-link']}`} target='_blank'>
+      <div className={`${styles['project-tile']}`}>
+        <h2 className={`${styles['project-name']}`}>freeCodeCamp Certification Projects</h2>
+      </div>
+    </a>
     <a href="https://github.com/TonyBanuchi/freeCodeCamp-Projects/tree/master/responsiveWebDesign/tributePage"
-      className={`${styles['project-link']}`}>
+      className={`${styles['project-link']}`} target='_blank'>
       <div className={`${styles['project-tile']}`}>
         <h2 className={`${styles['project-name']}`}>Tribute Page</h2>
       </div>
     </a>
     <a href="https://github.com/TonyBanuchi/freeCodeCamp-Projects/tree/master/responsiveWebDesign/surveyForm"
-      className={`${styles['project-link']}`}>
+      className={`${styles['project-link']}`} target='_blank'>
       <div className={`${styles['project-tile']}`}>
         <h2 className={`${styles['project-name']}`}>Survey Form</h2>
       </div>
     </a>
     <a href="https://github.com/TonyBanuchi/freeCodeCamp-Projects/tree/master/responsiveWebDesign/technicalDocumentationPage"
-      className={`${styles['project-link']}`}>
+      className={`${styles['project-link']}`} target='_blank'>
       <div className={`${styles['project-tile']}`}>
         <h2 className={`${styles['project-name']}`}>Technical Documentation Page</h2>
       </div>
     </a>
     <a href="https://github.com/TonyBanuchi/freeCodeCamp-Projects/tree/master/responsiveWebDesign/productLandingPage"
-      className={`${styles['project-link']}`}>
+      className={`${styles['project-link']}`} target='_blank'>
       <div className={`${styles['project-tile']}`}>
         <h2 className={`${styles['project-name']}`}>Product Landing Page</h2>
       </div>
     </a>
-    <a href="https://github.com/TonyBanuchi/freeCodeCamp-Projects/tree/master/responsiveWebDesign/personalPortflio"
-      className={`${styles['project-link']}`}>
+    <a href="https://github.com/TonyBanuchi/freeCodeCamp-Projects/tree/master/responsiveWebDesign/personalPortfolio"
+      className={`${styles['project-link']}`} target='_blank'>
       <div className={`${styles['project-tile']}`}>
         <h2 className={`${styles['project-name']}`}>Portfolio Page</h2>
       </div>
     </a>
-    <a href="https://github.com/TonyBanuchi/freeCodeCamp-Projects/tree/master" className={`${styles['projects-link']}`} id="link-to-all">
+    <a href="https://github.com/TonyBanuchi?tab=repositories" className={`${styles['projects-link']}`} id="link-to-all"  target='_blank'>
       <div className={`${styles['project-tile']}`}>
         <h2 className={`${styles['project-name']}`}>See All ... </h2>
       </div>
